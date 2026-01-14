b5220ec704f7879356e7df4faa63700693dbb499
hi <115565725+Mikeblackbelt@users.noreply.github.com>
2026-01-14 14:01:40 -0500

    d

--- calls/test.py
+++ calls/test.py
@@ -1,21 +1,67 @@
-import time
-import swagger_client
-from swagger_client.rest import ApiException
-from pprint import pprint
-import os
+# You will use this to log in to your Strava account
+import webbrowser
+import json
 import dotenv
+import os
+import pprint
+from stravalib.client import Client
 
+# Open the secrets file and store the client ID and client secret as objects, separated by a comma
+# Read below to learn how to set up the app that provides you with the client ID
+# and the client secret
 dotenv.load_dotenv()
-    
-# Configure OAuth2 access token for authorization: strava_oauth
-swagger_client.configuration.access_token = os.getenv("token")
+client_id = os.getenv("id")
+client_secret = os.getenv("secret")
 
-# create an instance of the API class
-api_instance = swagger_client.AthletesApi()
 
-try: 
-    # Get Authenticated Athlete
-    api_response = api_instance.getLoggedInAthlete()
-    pprint(api_response)
-except ApiException as e:
-    print("Exception when calling AthletesApi->getLoggedInAthlete: %s\n" % e)
+# Create a client object
+client = Client()
+# Define your scope (this is read-only - see below for a "write" example which
+# allows you to update activities and publish new activities to your Strava account).
+# read_all allows read access for both private and public activities
+request_scope = ["read_all", "profile:read_all", "activity:read_all"]
+
+# Create a localhost URL for authorization (for local development)
+redirect_url = "http://127.0.0.1:5000/authorization"
+
+# Create authorization url; your app client_id required to authorize
+url = client.authorization_url(
+    client_id=client_id,
+    redirect_uri=redirect_url,
+    scope=request_scope,
+)
+
+# Open the URL in a web browser
+webbrowser.open(url)
+
+print(
+    """You will see a url that looks like this. """,
+    """http://127.0.0.1:5000/authorization?state=&code=12323423423423423423423550&scope=read,activity:read_all,profile:read_all,read_all")""",
+    """Copy the values between code= and & in the url that you see in the
+ browser. """,
+)
+# Using input allows you to copy the code into your Python console
+# (or Jupyter Notebook)
+code = input("Please enter the code that you received: ")
+print(
+    f"Great! Your code is {code}\n"
+    "Next, I will exchange that code for a token.\n"
+    "I only have to do this once."
+)
+
+# Exchange the code returned from Strava for an access token
+token_response = client.exchange_code_for_token(
+    client_id=client_id, client_secret=client_secret, code=code
+)
+
+token_response
+# Example output of token_response
+# {'access_token': 'value-here-123123123', 'refresh_token': # '123123123',
+# 'expires_at': 1673665980}
+
+# Get current athlete details
+athlete = client.get_athlete()
+# Print athlete name :) If this works, your connection is successful!
+print(f"Hi, {athlete.firstname} Welcome to stravalib!")
+pprint.pprint(vars(athlete))
+# You are now successfully authenticated!
